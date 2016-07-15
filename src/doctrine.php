16beddc30f478bb5d35b8d5162b782c206deb5aa
Deyvison_Rodrigo <deyvison_rodrigo@hotmail.com>
2016-07-15 16:23:20 -0300

    Doctrine configurado!

--- src/doctrine.php
+++ src/doctrine.php
@@ -2,6 +2,8 @@
 
 /**
  * Description of doctrine
+ * 
+ * Arquivo de configuração do doctrine.
  *
  * @author Deyvison Rodrigo B. Estevam <deyvison_rodrigo@hotmail.com>
  * @date Jul 14, 2016
@@ -18,8 +20,17 @@ $path = array(
 $isDevMode = true;
 
 $dbParams = array(
-    'driver'    =>  'pdo-mysql',
-    'deyvison'  =>  'vida',
+    'driver'    =>  'pdo_mysql',    
+    'user'      =>  'deyvison',
+    'password'  =>  'vida',
     'dbname'    =>  'doctrine_basico_curso',
 );
 
+$config = Setup::createAnnotationMetadataConfiguration($path, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+
+function getEntityManager(){
+    global $entityManager;
+    
+    return $entityManager;
+}
