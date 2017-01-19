d3825d50af2f58fddc7c49407829e4922b1d0df2
deyvison <deyvison@deyvison-linus>
2017-01-18 21:40:21 -0300

    Terminando configuração e criando sistema de rotas

---
+++ src/routerList.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Chamando EntityManager
+
+$entityManager = getEntityManager();
+
+//Criando Rotas
+
+$map->get('home', '/', function($request, $response) use($view, $entityManager){
+    return $view->render($response, 'home.phtml', [
+        'teste'=> 'slim php view funcionando!!@!'
+    ]);    
+});
+
+$map->get('categories.list', '/categories', function($request, $response) use($view, $entityManager){
+    $repository = $entityManager->getRepository(Category::class);
+    $categories = $repository->findAll();
+    
+    return $view->render($response, 'categories/list.phtml', [
+        'categories'    => $categories
+    ]);    
+});
---
+++ src/routers.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use Zend\Diactoros\Response;
+use Aura\Router\RouterContainer;
+use Zend\Diactoros\ServerRequestFactory;
+use Slim\Views\PhpRenderer;
+
+$ajax = false;
+
+//Boas práticas dizem que variáveis do tipo ' $_ ' não devem ser acessadas diretamente.
+$request = ServerRequestFactory::fromGlobals(
+    $_SERVER, $_COOKIE, $_REQUEST, $_POST, $_GET, $_FILES
+);
+
+//Container para routas, armazenará rotas
+$routerContainer = new RouterContainer();
+
+//Auxiliará na criação de rotas
+$map = $routerContainer->getMap();
+
+$view = new PhpRenderer(__DIR__."/../templates/");
+
+//incluindo rotas
+include __DIR__."/routerList.php";
+
+//Instanciando combinador...
+$matcher = $routerContainer->getMatcher();
+
+//Verifica se o request combina com alguma rota, se não combinar, recebe false.
+$router = $matcher->match($request);
+
+foreach ($router->attributes as $key => $value) {
+    $request = $request->withAttribute($key, $value);
+}
+
+$callable = $router->handler;
+
+/*@var $response Response*/
+$response = $callable($request, new Response());
+
+if($ajax){
+    echo $response->getBody();
+}else{
+    include __DIR__. "/../templates/header.phtml";
+        echo $response->getBody();
+    include __DIR__. "/../templates/footer.phtml";
+}
